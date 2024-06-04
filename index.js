b1dcf4b4f0334374020964c4a33e573e89371d3b
Barrysharifi <barrysharifi@gmail.com>
2024-06-03 20:23:28 -0400

    fixed css to make the grid work, and added an event listener with the click to add color to the grid items

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 let container = document.getElementById("container")
-for (let i = 0; i < 16; i++){
+for (let i = 0; i < 16; i++) {
     const rowContainer = document.createElement('div');
     rowContainer.className = "row-container";
     rowContainer.id = "row" + "-" + i;
     container.appendChild(rowContainer)
-    
-    for (let j = 0; j <16; j++){
+
+    for (let j = 0; j < 16; j++) {
         const gridItem = document.createElement('div')
         gridItem.className = "grid-item"
-        gridItem.id = "column - " + j
+        gridItem.id = "column-" + j
         rowContainer.appendChild(gridItem)
+
+
+        gridItem.addEventListener("click", () => {
+            let changeBoxColor = document.createElement('div')
+            changeBoxColor.className = "new-box-color"
+            gridItem.appendChild(changeBoxColor)
+
+        })
+
+
     }
-    
+
+
 }
+
+
+
+
+
